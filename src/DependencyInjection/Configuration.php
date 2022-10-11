01146bd703a0b791a1c3c81cea602ecd4285fe03
arasolutions <arasolutionsweb@gmail.com>
2022-10-11 17:16:33 +0200

    Update Configuration.php

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -13,28 +13,39 @@ class Configuration implements ConfigurationInterface
 
         $treeBuilder->getRootNode()
             ->children()
-                ->arrayNode('kiwi_cloud_cms')
-                    ->children()
-                        ->arrayNode('account')
-                            ->children()
-                                ->scalarNode('api_key')->end()
-                            ->end()
-                        ->end() // account
-                        ->arrayNode('cache')
-                            ->children()
-                                ->scalarNode('feeds')->end()
-                                ->scalarNode('folder')->end()
-                                ->integerNode('time')->end()
-                            ->end()
-                        ->end() // cache
-                        ->arrayNode('log')
-                            ->children()
-                                ->scalarNode('file')->end()
-                            ->end()
-                        ->end() // log
-                    ->end()
-                ->end() // kiwi_cloud_cms
-            ->end();
+            ->arrayNode('twitter')
+            ->children()
+            ->integerNode('client_id')->end()
+            ->scalarNode('client_secret')->end()
+            ->end()
+            ->end() // twitter
+            ->end()
+        ;
+
+//        $treeBuilder->getRootNode()
+//            ->children()
+//                ->arrayNode('kiwi_cloud_cms')
+//                    ->children()
+//                        ->arrayNode('account')
+//                            ->children()
+//                                ->scalarNode('api_key')->end()
+//                            ->end()
+//                        ->end() // account
+//                        ->arrayNode('cache')
+//                            ->children()
+//                                ->scalarNode('feeds')->end()
+//                                ->scalarNode('folder')->end()
+//                                ->integerNode('time')->end()
+//                            ->end()
+//                        ->end() // cache
+//                        ->arrayNode('log')
+//                            ->children()
+//                                ->scalarNode('file')->end()
+//                            ->end()
+//                        ->end() // log
+//                    ->end()
+//                ->end() // kiwi_cloud_cms
+//            ->end();
 
         return $treeBuilder;
     }
