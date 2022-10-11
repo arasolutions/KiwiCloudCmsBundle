179baf537fb8a69cd1bd712e2ece7f5df3da87f0
arasolutions <arasolutionsweb@gmail.com>
2022-10-11 09:48:33 +0200

    Update Configuration.php

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -13,8 +13,27 @@ class Configuration implements ConfigurationInterface
 
         $treeBuilder->getRootNode()
             ->children()
-            ->scalarNode('version')->end()
-            ->end()
+                ->arrayNode('kiwi_cloud_cms')
+                    ->children()
+                        ->arrayNode('account')
+                            ->children()
+                                ->scalarNode('api_key')->end()
+                            ->end()
+                        ->end() // account
+                        ->arrayNode('cache')
+                            ->children()
+                                ->scalarNode('feeds')->end()
+                                ->scalarNode('folder')->end()
+                                ->integerNode('time')->end()
+                            ->end()
+                        ->end() // cache
+                        ->arrayNode('log')
+                            ->children()
+                                ->scalarNode('file')->end()
+                            ->end()
+                        ->end() // log
+                    ->end()
+                ->end() // kiwi_cloud_cms
             ->end();
 
         return $treeBuilder;
