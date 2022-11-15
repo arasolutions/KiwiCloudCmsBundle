80262b84be19053d46efa9895cde0c039e54dc8b
Romain Da Silva <romain.rdasilva@gmail.com>
2022-11-15 15:15:48 +0100

    avancement

--- src/DependencyInjection/KiwiCloudCmsExtension.php
+++ src/DependencyInjection/KiwiCloudCmsExtension.php
@@ -17,7 +17,6 @@ class KiwiCloudCmsExtension extends Extension
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         
         try {
-            $loader->load('kiwi_cloud_cms.yaml');
             $loader->load('services.yaml');
         } catch (Exception $e) {
             throw new Exception($e->getMessage());
@@ -25,6 +24,7 @@ class KiwiCloudCmsExtension extends Extension
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
         foreach ($config as $key => $value) {
             $container->setParameter('kiwi_cloud_cms.' . $key, $value);
         }
